d7e7d3ce5633ef9c2cd845e35e0e300f0305e595
Lukichi-ve <lucasve45@gmail.com>
2025-08-28 04:32:28 -0300

    Cambios en el responsive y navbar

--- js/nav.js
+++ js/nav.js
@@ -1,10 +1,9 @@
-const botonCerrarMenu = document.querySelector('.close-menu');
 const botonAbrirMenu = document.querySelector('.open-menu');
 
 botonAbrirMenu.addEventListener('click', () => {
     document.getElementById('mobile-navbar').style.width = '50%';
 });
 
-botonCerrarMenu.addEventListener('click', () => {
+function cerrarMenu(){
     document.getElementById('mobile-navbar').style.width = '0%';
-});
+};
